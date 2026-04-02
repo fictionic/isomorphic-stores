21c27f8ffe1e237e59cdfcb6a9803e910d57ade1
Dylan Forbes <dylandforbes@gmail.com>
2026-04-01 18:08:27 -0700

    update valtio adapter to allow access to the proxy in the store constructor for onMessage

--- packages/store-adapter-valtio/src/valtio.ts
+++ packages/store-adapter-valtio/src/valtio.ts
@@ -1,5 +1,5 @@
 import {defineIsoStore, type Adapter, type IsoStoreInit} from "@verso-js/stores/adapter";
-import {proxy, useSnapshot, type Snapshot} from "valtio";
+import {proxy as valtioProxy, useSnapshot, type Snapshot} from "valtio";
 
 export interface ValtioHooks<State> {
   useProxy: () => State;
@@ -11,24 +11,40 @@ export interface ValtioClientHooks<State> {
   useSnapshot: () => Snapshot<State> | undefined;
 };
 
-const empty = proxy({});
+export type ValtioInit<State> = (getProxy: () => State) => State;
+
+const empty = valtioProxy({});
 
 /**
  * Valtio doesn't fit as cleanly into this machinery. Instead of talking directly to the proxy by
  * importing it, components access it via a useProxy hook that the store definition provides, alongside
  * useSnapshot. Maybe a little weird, but basically the same.
- * Note that the first three parameters are the same because the store instance _is_ the store state (it's
- * a proxy), and there is no store constructor--the user just passes the initial state into proxy().
+ * Note that the first two type parameters are the same because the store instance _is_ the store state--it's
+ * a proxy. And the store structor is a function only to allow consumers to call onMessage: there needs to
+ * be a way to access the proxy within the constructor because that's the only way to mutate the state,
+ * but the constructor is what creates the proxy, so there's a chicken-and-egg problem. The only solution
+ * is to provide a getter that is wired up after the fact.
  */
 export const getAdapter: <State extends object>() => Adapter<
   State,
   State,
-  State,
+  ValtioInit<State>,
   ValtioHooks<State>,
   ValtioClientHooks<State>
 > = <State extends object>() => {
   return {
-    createNativeStore: (initialState: State) => proxy(initialState),
+    createNativeStore: (init: ValtioInit<State>) => {
+      let proxy: State | null = null;
+      const getProxy = () => {
+        if (!proxy) {
+          throw new Error("proxy not yet created!");
+        }
+        return proxy;
+      };
+      const initialState = init(getProxy);
+      proxy = valtioProxy(initialState);
+      return proxy;
+    },
     getSetState: (proxy) => (
       (state) => Object.assign(proxy, state)
     ),
@@ -55,6 +71,6 @@ export const getAdapter: <State extends object>() => Adapter<
 };
 
 export const defineValtioIsoStore = <Opts, State extends object, Message = never>(
-  isoInit: IsoStoreInit<Opts, State, Message, State>,
+  isoInit: IsoStoreInit<Opts, State, Message, ValtioInit<State>>,
   options?: { onError?: (error: unknown) => void },
 ) => defineIsoStore(isoInit, getAdapter<State>(), options);
